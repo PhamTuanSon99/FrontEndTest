1c53dacb6c0aefe62c088e7a0b43681978284f0d
PhamTuanSon99 <phamtuanson9x@gmail.com>
2021-06-18 09:24:23 +0700

    .

---
+++ require-config.js
@@ -0,0 +1,10 @@
+var config = {
+    paths: {            
+         'myjs': "AHT_FrontEndTest/js/authentication-popup"
+      },   
+    shim: {
+    'myjs': {
+        deps: ['jquery']
+    }
+  }
+}
